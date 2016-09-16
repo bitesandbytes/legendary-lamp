2991f0a6e68170489d983c051e2ce6c2c4c9c76f
SaipraveenB <cs13b025@smail.iitm.ac.in>
2016-09-16 19:51:39 +0530

    Added extra comments

--- SpaceTransform.cpp
+++ SpaceTransform.cpp
@@ -9,7 +9,7 @@ std::vector<std::tuple<std::string, float, bool> > SpaceTransform::applyAll(std:
   // Use this->words.count( word ) to check if the word is in the dictionary.
   // Use this->probability to get the probability of space.
 
-
-  // Return a set of candidates along with their probabilities.
+  // Return a set of candidates along with their probabilities. The boolean should be True for a valid condidate and False for an invalid( but potentially valid ) candidate.
   return std::vector<std::tuple<std::string, float, bool>>();
+
 }
--- Transform.cpp
+++ Transform.cpp
@@ -14,6 +14,14 @@ void Transform::setWords(Distribution<std::string> *words) {
 }
 std::vector<std::tuple<std::string, float, bool> > Transform::applyAll(std::string str) const {
 
+  // Use this->words[word] to get probabilities for the words.
+  // Use this->words.count( word ) to check if the word is in the dictionary.
+  // Use this->probability to get the probability of this transform.
+
+
+  // Return a set of candidates along with their probabilities. The boolean should be True for a valid condidate and False for an invalid( but potentially valid ) candidate.
+  // Do not consider multiple instances of the transform.
+  // For example if the transform is "ab"->"a" and "ab" occurs twice, then there should be 2 candidates( one for each separately, but none for both together )
   return std::vector<std::tuple<std::string, float, bool>>();
 }
 
